559cdf066845cc3f6bd2797f6d0757139eb5de9d
Murilo <murilo.barross@sptech.school>
2024-12-03 23:42:15 -0300

    testando jar

--- src/main/java/sptech/school/ConexBanco.java
+++ src/main/java/sptech/school/ConexBanco.java
@@ -1,25 +1,28 @@
 package sptech.school;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.logging.Log;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ConexBanco {
     private JdbcTemplate conexBanco;
 
     public ConexBanco() {
-        BasicDataSource configBanco = new BasicDataSource();
+        try {
+            BasicDataSource configBanco = new BasicDataSource();
+            configBanco.setUrl("jdbc:mysql://34.200.9.69/aevus");
+            configBanco.setUsername("devaevus");
+            configBanco.setPassword("aevus123");
+            configBanco.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
-        // Configurações da conexão com MySQL
-        configBanco.setUrl("jdbc:mysql://34.200.9.69/aevus"); // Altere para o seu IP e nome do banco
-        configBanco.setUsername("devaevus"); // Altere para o seu nome de usuário
-        configBanco.setPassword("aevus123"); // Altere para a sua senha
-
-        // Configura o driver do MySQL
-        configBanco.setDriverClassName("com.mysql.cj.jdbc.Driver"); // Adicione esta linha
-
-        // Associa o DataSource ao JdbcTemplate
-        this.conexBanco = new JdbcTemplate(configBanco);
+            this.conexBanco = new JdbcTemplate(configBanco);
+        } catch (Exception e) {
+            Log logger = null;
+            logger.error("Erro ao configurar o banco de dados: " + e.getMessage());
+            throw new RuntimeException("Erro ao configurar o banco de dados", e);
+        }
     }
+
 
     // Retorna o JdbcTemplate configurado
     public JdbcTemplate getConexaoBanco() {
